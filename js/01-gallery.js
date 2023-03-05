e438ee74be5256304234c5e3869980b7142b8b82
MargaritaSukhova <margarytasu@gmail.com>
2023-03-05 20:30:25 +0200

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(photos) {
+	return photos
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+					<a class="gallery__link" href="${original}">
+						<img
+							class="gallery__image"
+							src="${preview}"
+							data-source="${original}"
+							alt="${description}"
+						/>
+					</a>
+				</div>`;
+		})
+		.join("");
+}
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryEl.addEventListener("click", handlePhotoClick);
+
+function handlePhotoClick(event) {
+	event.preventDefault();
+
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const url = event.target.dataset.source;
+
+	const instance = basicLightbox.create(`
+    <img src="${url}">
+`);
+
+	instance.show();
+}
